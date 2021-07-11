3a1e958b730052548d42bd3fb23106820bc78fc2
arsenbig <arsenbig@gmail.com>
2021-07-12 00:30:32 +0400

    Initial

---
+++ terraform/config.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  profile                 = "default"
+  region                  = "us-east-2"
+  access_key = "AKIAXTSMAVRGV4K5CUMD"
+  secret_key = "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"
+}
